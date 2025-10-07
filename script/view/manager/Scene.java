b932853479042704dca404c63742d6330001ba5d
Nicolas Bouillon <nicolas.bouillon.pro1@gmail.com>
2025-10-07 11:32:25 +0200

    Scene

--- script/view/manager/Scene.java
+++ script/view/manager/Scene.java
@@ -1,14 +1,22 @@
 package view;
 
+import javafx.application.Application;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
-public abstract class Scene {
+public abstract class Scene extends Application{
 
     private Pane[] element;
 
-    public void dysplay() {}
+    public void display() {}
 
     public void update() {}
+
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Morpion");
+        primaryStage.setScene(new Scene(new Pane(), 800, 600));
+        primaryStage.show();
+    }
 
 }
