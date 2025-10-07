11aaf1027e2a5592fbc47cd151c74b6df515076d
Ver5Gtorix <florentindeum@gmail.com>
2025-10-07 09:59:32 +0200

    Declaration of the class Scene and ViewManager

--- script/view/manager/Scene.java
+++ script/view/manager/Scene.java
@@ -1,5 +1,15 @@
 package view;
 
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public abstract class Scene {
 
+    private Pane[] element;
+
+    public Scene(Pane[] element) {}
+
+    public void dysplay() {}
+
+    public void update() {}
 }
--- script/view/manager/ViewManager.java
+++ script/view/manager/ViewManager.java
@@ -1,5 +1,23 @@
 package view;
 
+import javafx.stage.Stage;
+
 public class ViewManager {
+
+    private Stage primaryStage;
+
+    private Scene scene;
+
+    public ViewManager(Stage primaryStage) {}
+
+    public void showCreateGameScene(){}
+
+    public void showMainGameScene() {}
+
+    public void showSettingsScene() {}
+
+    public void showPauseScene() {}
+
+    public void showEndingScene() {}
 
 }
