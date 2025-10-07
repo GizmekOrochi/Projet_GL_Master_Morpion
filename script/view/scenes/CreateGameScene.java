37b72680cfe3604dcdaaca9da7dc88a1f08df3f6
Nicolas Bouillon <nicolas.bouillon.pro1@gmail.com>
2025-10-07 09:55:14 +0200

    declaration mainGameScene and CreateGameScene

--- script/view/scenes/CreateGameScene.java
+++ script/view/scenes/CreateGameScene.java
@@ -2,4 +2,30 @@ package view;
 
 public class CreateGameScene {
 
+    public CreateGameScene() {
+        // Constructor logic
+    }
+
+    public int[][] getGrid() {
+        // Return the current state of the game board
+        return new int[3][3];
+    }
+
+    public int getWinCondtion() {
+        // Return the win condition (e.g., number of marks in a row needed to win)
+        return 3;
+    }
+
+    public int getPlayerNb() {
+        // Return the name of player 1
+        return 1;
+    }
+
+    public void lauchGame() {
+        // Logic to launch the game
+    }
+
+    public void openSettingMenu() {
+        // Logic to open the settings scene
+    }
 }
--- script/view/scenes/MainGameScene.java
+++ script/view/scenes/MainGameScene.java
@@ -2,4 +2,27 @@ package view;
 
 public class MainGameScene {
 
+    public MainGameScene() {
+    }
+
+    public void updateTurnIndicator(String playerName) {
+    }
+
+    public void placeSymbol(int x, int y, String symbol) {
+    }
+
+    public void removeSymbol(int x, int y) {
+    }
+
+    public void openSettingMenu() {
+    }
+    
+    public void openPauseMenu(){
+    }
+
+    public void openEndingScene() {
+    }
+
+
 }
+
