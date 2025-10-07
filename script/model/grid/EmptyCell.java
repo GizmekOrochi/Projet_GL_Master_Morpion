39af02afd1405c97b7da644b7d46ca65e830e6f2
GizmekOrochi <valcurion369@gmail.com>
2025-10-07 16:35:24 +0200

    makefile ( for model compliation )

--- script/model/grid/EmptyCell.java
+++ script/model/grid/EmptyCell.java
@@ -1,5 +1,10 @@
 package model.grid;
 
 public class EmptyCell extends CellContent {
+
     // Pas d'attribut supplémentaire selon le diagramme
+    public EmptyCell(int x, int y) {
+        super(x, y);
+    }
+    
 }
